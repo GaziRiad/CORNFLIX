58301c366ea9199471c0413ecb21f2bc058e0be7
GaziRiad <ouhibimustapha36@gmail.com>
2023-09-02 17:34:19 +0100

    Adding tailwindcss to project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { useEffect } from "react";
 
-function App() {
+const API_KEY = "db3cf4891cc843bd89f697bffe4118cc";
+
+// fetch POPULAR
+// const res = await fetch(
+//   `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc`
+// );
+
+// fetch MOVIE
+// const res = await fetch(
+//   `https://api.themoviedb.org/3/movie/654865?api_key=${API_KEY}&language=en-US`
+// );
+
+export default function App() {
+  useEffect(() => {
+    async function fetchPopularMovies() {
+      // 654865
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/654865?api_key=${API_KEY}&language=en-US`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchPopularMovies();
+  }, []);
+
   return (
-    <div></div>
+    <div>
+      <Header />
+    </div>
   );
 }
 
-export default App;
+function Header() {
+  return <header className="text-blue-700 font-bold">CORNFLIX</header>;
+}
