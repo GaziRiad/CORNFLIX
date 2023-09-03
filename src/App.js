73b88baa780013a9cdcad10c184987039e9907f9
GaziRiad <ouhibimustapha36@gmail.com>
2023-09-03 11:52:15 +0100

    Adding the popular movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
-import { FeaturedMovie } from "./FeaturedMovie";
+import FeaturedMovie from "./FeaturedMovie";
 
 export const API_KEY = "db3cf4891cc843bd89f697bffe4118cc";
 
@@ -15,30 +15,65 @@ export const API_KEY = "db3cf4891cc843bd89f697bffe4118cc";
 // );
 
 export const baseImageUrl = "https://image.tmdb.org/t/p/";
-// const imageSize = 'w500';
-
-const posterPath = "/drCySAAAvegq1vQRGRqPKN9f00w.jpg";
 
 export default function App() {
-  useEffect(() => {
-    async function fetchPopularMovies() {
-      // 654865
-      const res = await fetch(
-        `https://api.themoviedb.org/3/movie/654865?api_key=${API_KEY}&language=en-US`
-      );
-      const data = await res.json();
-      console.log(data);
-    }
-  }, []);
+  const [featuredMovie, setFeaturedMovie] = useState({});
+  const [Movies, setPopularMovies] = useState([]);
 
+  useEffect(() => {
+    async function fetchMovie() {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc`
+        );
+        const data = await res.json();
+        const featured = data.results[0];
+        setFeaturedMovie(featured);
+        setPopularMovies(data.results);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchMovie();
+  }, [setFeaturedMovie, setPopularMovies]);
   return (
     <div>
       <Header />
-      <FeaturedMovie />
+      <FeaturedMovie movie={featuredMovie} />
+      <PopularMovies movies={Movies} />
     </div>
   );
 }
 
-function PopularMovies() {
-  return <div></div>;
+function PopularMovies({ movies }) {
+  return (
+    <div className=" w-[70%] mx-auto py-12">
+      <h2 className=" font-bold text-[#000411] uppercase text-xl mb-6">
+        Popular movies
+      </h2>
+      <div className=" grid grid-cols-1 gap-x-10 gap-y-10 md:grid-cols-5">
+        {movies &&
+          movies.map((movie) => {
+            return (
+              <div
+                key={movie.id}
+                className=" w-[100%] h-96 rounded-md overflow-hidden  text-white hover:brightness-75 duration-500 cursor-pointer"
+              >
+                <img
+                  src={`${baseImageUrl}w400${movie.poster_path}`}
+                  alt=""
+                  className="h-[80%] w-[100%]"
+                />
+                <p className="h-[20%] text-center bg-slate-900 p-4">
+                  {movie.title.split(" ").length > 6
+                    ? movie.title.split(" ").slice(0, 4).join(" ") + " ..."
+                    : movie.title}
+                </p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
--- src/FeaturedMovie.js
+++ src/FeaturedMovie.js
@@ -1,34 +1,18 @@
-import { useEffect, useState } from "react";
-import { API_KEY, baseImageUrl } from "./App";
+import { baseImageUrl } from "./App";
 
-export function FeaturedMovie() {
-  const [featuredMovie, setFeaturedMovie] = useState({});
-
-  useEffect(() => {
-    async function fetchMovie() {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc`
-      );
-      const data = await res.json();
-      const featured = data.results[0];
-      console.log(featured);
-      setFeaturedMovie(featured);
-    }
-    fetchMovie();
-  }, [setFeaturedMovie]);
-
+export default function FeaturedMovie({ movie }) {
   return (
-    <div className="relative h-screen">
+    <div className="relative h-[60vh]">
       <img
-        src={`${baseImageUrl}w500${featuredMovie.backdrop_path}`}
+        src={`${baseImageUrl}w500${movie.backdrop_path}`}
         alt=""
-        className="h-2/3 w-full filter brightness-50"
+        className="h-full w-full filter brightness-50"
       />
       <p className="absolute top-80 left-24 font-bold text-white text-3xl uppercase">
-        {featuredMovie.title}
+        {movie.title}
       </p>
       <p className="absolute top-96 left-6 font-normal leading-7 text-white text-lg md:w-2/4 md:left-24 md:text-xl md:leading-8">
-        {featuredMovie.overview}
+        {movie.overview}
       </p>
     </div>
   );
